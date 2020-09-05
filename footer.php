beb09bb3a9150773d693a7c592c34b628728625b
Alex Boeger <alexboeger@gmail.com>
2020-09-05 13:07:32 -0500

    Added customer footerfull for full width continaer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part( 'footerfull' ); ?>
 
 <div class="wrapper" id="wrapper-footer">
 
